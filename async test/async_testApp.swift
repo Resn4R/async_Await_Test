415bb085721a171c0a68fdca40839758d2c0dbc3
Resn4R <purview-joust.0x@icloud.com>
2023-09-27 14:32:41 +0100

    Initial Commit

---
+++ async test/async_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  async_testApp.swift
+//  async test
+//
+//  Created by Vito Borghi on 27/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct async_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
